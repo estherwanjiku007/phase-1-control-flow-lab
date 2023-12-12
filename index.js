8029c41a790933e665a0f0b10fc936e9204c5fed
ESTHER WANJIKU <ESTHERWANJIQU@GMAIL.COM>
2023-12-12 15:47:24 +0300

    aster

--- index.js
+++ index.js
@@ -1,31 +1,25 @@
 function scuberGreetingForFeet(ride){
-  let ride=199;
      if(ride<=400){
-      console.log("Free sample")
+      return "This one is on me!";
     }
-    else if(ride>=2000){
-      console.log("I will gladly take your 30 bucks.")
+    else if(ride>=2000 && ride <2500){
+      return "I will gladly take your thirty bucks.";
     }
-    else {
-      console.log("No can do.")
+    else if(ride>2500){
+      return "No can do.";
     }
   }
 function ternaryCheckCity(city){
-  let city="NYC"
-    if (city="NYC"){
-      console.log("ok, sounds good.")
-    }
-    else{
-      console.log("No go.")
-    }
-  }
+   return city ==="NYC" ?"Ok, sounds good.":"No go.";
+}
 function switchOnCharmFromTip(tip){
-  let tip="generous"
-  if (tip="generous"){
-    console.log("Thank you.")
-  }
-  else{
-    console.log("Bye.")
-
+  switch (tip){
+    case "generous":
+      return "Thank you so much.";
+      case "not as generous":
+        return "Thank you.";
+        default:
+          return "Bye.";
   }
 }
+  
