a538b226f07a7c369ef380c5dec2cbd71e37b3cc
ESTHER WANJIKU <ESTHERWANJIQU@GMAIL.COM>
2023-12-12 14:19:17 +0300

    aster

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(ride){
+  let ride=199;
+     if(ride<=400){
+      console.log("Free sample")
+    }
+    else if(ride>=2000){
+      console.log("I will gladly take your 30 bucks.")
+    }
+    else {
+      console.log("No can do.")
+    }
+  }
+function ternaryCheckCity(city){
+  let city="NYC"
+    if (city="NYC"){
+      console.log("ok, sounds good.")
+    }
+    else{
+      console.log("No go.")
+    }
+  }
+function switchOnCharmFromTip(tip){
+  let tip="generous"
+  if (tip="generous"){
+    console.log("Thank you.")
+  }
+  else{
+    console.log("Bye.")
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+  }
 }
